97181b3e14dbf079ff90d52c80dd2df3337ff334
Wilfried AGO <wilfriedago@pm.me>
2024-03-23 01:00:24 +0100

    chore: remove codeium.nvim plugin and update lazy-lock.json

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,7 +17,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- import/override with your plugins
